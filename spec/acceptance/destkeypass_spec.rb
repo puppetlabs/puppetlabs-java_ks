3323cf62cde4ecee8bf360787ee2b11e8ae18140
Travis Fields <travis@puppetlabs.com>
2015-09-10 16:29:04 -0700

    (FM-3640) Fix issue with destkeypass spec
    - Was not using shared context and temp variables
      moved to use shared context

--- spec/acceptance/destkeypass_spec.rb
+++ spec/acceptance/destkeypass_spec.rb
@@ -3,27 +3,27 @@ require 'spec_helper_acceptance'
 hostname = default.node_name
 
 describe 'password protected java private keys', :unless => UNSUPPORTED_PLATFORMS.include?(fact('operatingsystem')) do
+  include_context 'common variables'
+ 
   let(:confdir)    { default['puppetpath']    }
   let(:modulepath) { default['distmoduledir'] }
+  
   case fact('osfamily')
-  when "Solaris"
-    keytool_path = '/usr/java/bin/'
-    resource_path = "['/usr/java/bin/','/opt/puppet/bin/', '/usr/bin']"
-  when "AIX"
-    keytool_path = '/usr/java6/bin/'
-    resource_path = "['/usr/java6/bin/','/usr/bin/']"
-  else
-    resource_path = "undef"
+    when "windows"
+      target = 'c:/private_key.ks'
+    else
+      target = '/etc/private_key.ks'
   end
+  
   it 'creates a password protected private key' do
     pp = <<-EOS
-      java_ks { 'broker.example.com:/etc/private_key.ks':
+      java_ks { 'broker.example.com:#{target}':
         ensure       => latest,
-        certificate  => "/tmp/ca.pem",
-        private_key  => "/tmp/privkey.pem",
+        certificate  => "#{@temp_dir}ca.pem",
+        private_key  => "#{@temp_dir}privkey.pem",
         password     => 'testpass',
         destkeypass  => 'testkeypass',
-        path         => #{resource_path},
+        path         => #{@resource_path},
       }
     EOS
 
@@ -31,16 +31,16 @@ describe 'password protected java private keys', :unless => UNSUPPORTED_PLATFORM
   end
 
   it 'can make a cert req with the right password' do
-    shell("#{keytool_path}keytool -certreq -alias broker.example.com -v "\
-     "-keystore /etc/private_key.ks -storepass testpass -keypass testkeypass") do |r|
+    shell("#{@keytool_path}keytool -certreq -alias broker.example.com -v "\
+     "-keystore #{target} -storepass testpass -keypass testkeypass") do |r|
       expect(r.exit_code).to be_zero
       expect(r.stdout).to match(/-BEGIN NEW CERTIFICATE REQUEST-/)
     end
   end
 
   it 'cannot make a cert req with the wrong password' do
-    shell("#{keytool_path}keytool -certreq -alias broker.example.com -v "\
-     "-keystore /etc/private_key.ks -storepass testpass -keypass qwert",
+    shell("#{@keytool_path}keytool -certreq -alias broker.example.com -v "\
+     "-keystore #{target} -storepass testpass -keypass qwert",
      :acceptable_exit_codes => 1)
   end
 
