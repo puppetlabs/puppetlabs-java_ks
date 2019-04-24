443a0c8944075c485f4e42f272aa7a1862f45a4c
lionce <loredana.ionce@puppet.com>
2019-04-24 15:16:00 +0300

    MODULES-8962 - java_ks - Windows 2012 failing smoke - fix java version

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -156,7 +156,7 @@ end
 
 RSpec.shared_context 'common variables' do
   before(:each) do
-    java_major, java_minor = (ENV['JAVA_VERSION'] || '8u201').split('u')
+    java_major, java_minor = (ENV['JAVA_VERSION'] || '8u211').split('u')
     @ensure_ks = 'latest'
     @resource_path = 'undef'
     @target_dir = '/etc/'
